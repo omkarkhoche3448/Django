6c3427b71aa6eed9a1cf03ae51755e2df086bb9a
omkarkhoche3448 <omkarkhoche3448@gmail.com>
2024-09-06 09:33:48 +0530

    -created  New template for framework details. - Updated URLs and views for detailed framework information.

--- DjangoProject/djangoApp/urls.py
+++ DjangoProject/djangoApp/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 # localhost:8000/djangoApp/items
 urlpatterns = [
-    path('items/', views.items, name='Items'), 
+    path('items/', views.items, name='Items'),
+    path('<int:framework_id>/', views.framework_details, name='framework_details'),
+ 
 ]
--- DjangoProject/djangoApp/views.py
+++ DjangoProject/djangoApp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import FrameWork_Varity
 
 # View for localhost:8000/djangoApp/items
@@ -6,7 +7,9 @@ def items(request):
     FrameWorks= FrameWork_Varity.objects.all()
     return render(request, 'djangoApp/items.html', {'FrameWorks':FrameWorks })
 
-
+def framework_details(request, framework_id):
+    framework = get_object_or_404(FrameWork_Varity, pk=framework_id)
+    return render(request, 'djangoApp/framework_details.html', {'framework': framework})
 
 
 
